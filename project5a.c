034e164fb56eb61980a53a571e04416f92aeec02
johnalexiv <johnalexiv@gmail.com>
2017-11-09 15:00:27 -0800

    Added command line parsing

--- project5a.c
+++ project5a.c
@@ -13,9 +13,71 @@
 #include <semaphore.h>
 #include <stdbool.h>
 
-int main()
+bool checkValidNumOfArguments(int);
+bool isDeadlockSimulation(char);
+bool isNormalSimulation(char);
+void deadLockSimulation();
+void normalSimulation();
+
+int main(int argc, int *argv[])
 {
-    
-    
+    if ( checkValidNumOfArguments(argc) )
+        exit(0);
+
+    if ( isDeadlockSimulation(argv[1][0]) )
+        deadLockSimulation();
+    else if ( isNormalSimulation(argv[1][0]) )
+        normalSimulation();
+
+    printf("there were two arguments\n");
+
     return 0;
 }
+
+bool checkValidNumOfArguments(int argc)
+{
+    if (argc != 2)
+        return true;
+    return false;
+}
+
+bool isDeadlockSimulation(char argument)
+{
+    if ( argument == '0' )
+        return true;
+    return false;
+}
+
+bool isNormalSimulation(char argument)
+{
+    if ( argument == '1' )
+        return true;
+    return false;
+}
+
+void deadLockSimulation()
+{
+    printf("deadlock\n");
+}
+
+void normalSimulation()
+{
+    printf("normal\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
